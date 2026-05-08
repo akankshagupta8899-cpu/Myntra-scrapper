e2e3df972388e9439ed1efd9d8228e43c971774e
Akanksha Gupta <akankshagupta8899@gmail.com>
2026-05-08 10:46:46 +0530

    initial folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="scrapper",
+    version="0.0.1",
+    author="akanksha",
+    author_email="akankshagupta8899@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+
+)
